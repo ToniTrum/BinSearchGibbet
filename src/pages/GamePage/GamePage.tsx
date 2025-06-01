b5e614c45c4bb52c6419a8180410f4e7d7d53e29
ToniTrum <kulahszjan@gmail.com>
2025-06-02 03:39:58 +1000

    add Gibbet

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,8 +1,9 @@
 import { NumberList } from "../../widgets/NumberList.tsx";
 import { GameOverModal } from "../../shared/ui/GameOverModal/GameOverModal.tsx";
-import { COUNT_NUMBERS } from "../../shared/constants";
+import { COUNT_NUMBERS, MAX_ATTEMPTS } from "../../shared/constants";
 import { GameHeader } from "../../widgets/GameHeader";
 import { useGuessNumber } from "../../processes/useGuessNumber";
+import Gibbet from "../../widgets/Gibbet/Gibbet.tsx";
 
 export const GamePage = () => {
   const { answer, selected, countAttempts, selectNumber, restartGame, gameKey } = useGuessNumber();
@@ -12,6 +13,10 @@ export const GamePage = () => {
       <GameHeader 
         selected={selected} 
         answer={answer} 
+      />
+      <Gibbet 
+        countAttempts={countAttempts} 
+        maxAttempts={MAX_ATTEMPTS} 
       />
       <GameOverModal 
         isOpen={countAttempts <= 0 || selected === answer} 
